7c76ec40643a53b3eb82705218a2ef9f1625720f
alejandro <avenegasc14@gmail.com>
2021-10-20 17:53:03 -0600

    Add logo

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -7,52 +7,12 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <StaticImage
-        backgroundColor={"rgb(97, 170, 165)"}
         width={56}
-        height={56}
-        src="../../../../images/icon.png"
+        src="../../../../images/logo.jpeg"
+        objectFit={"contain"}
+        loading={"eager"}
         alt="Logo"
-        className={styles.logo}
       />
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li>
-            <AnimLink className={styles.link} to={"/"}>
-              Inicio
-            </AnimLink>
-          </li>
-          <li>
-            <AnimLink className={styles.link} to={"/quienes-somos"}>
-              ¿Quiénes Somos?
-            </AnimLink>
-          </li>
-          <li>
-            <AnimLink className={styles.link} to={"/docentes"}>
-              Docentes
-            </AnimLink>
-          </li>
-          <li>
-            <AnimLink className={styles.link} to={"/experiencias-musicales"}>
-              Experiencias Musicales
-            </AnimLink>
-          </li>
-          <li>
-            <AnimLink className={styles.link} to={"/paquetes"}>
-              Paquetes
-            </AnimLink>
-          </li>
-          <li>
-            <AnimLink className={styles.link} to={"/preguntas-frecuentes"}>
-              Preguntas Frecuentes
-            </AnimLink>
-          </li>
-          <li>
-            <AnimLink className={styles.link} to={"/contactenos"}>
-              Contáctenos
-            </AnimLink>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
