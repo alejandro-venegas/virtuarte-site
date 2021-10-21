343a13a50eb0f385bb95f15fa4498ebeceed0016
alejandro <avenegasc14@gmail.com>
2021-10-20 21:52:17 -0600

    Fix nav

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import * as styles from "./index.module.scss";
-import AnimLink from "../../../shared/AnimLink";
 import { StaticImage } from "gatsby-plugin-image";
+import AnimLink from "../../../shared/AnimLink";
 const Header = () => {
   return (
     <header className={styles.header}>
@@ -13,6 +13,45 @@ const Header = () => {
         loading={"eager"}
         alt="Logo"
       />
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li>
+            <AnimLink className={styles.link} to={"/"}>
+              Inicio
+            </AnimLink>
+          </li>
+          <li>
+            <AnimLink className={styles.link} to={"/quienes-somos"}>
+              ¿Quiénes Somos?
+            </AnimLink>
+          </li>
+          <li>
+            <AnimLink className={styles.link} to={"/docentes"}>
+              Docentes
+            </AnimLink>
+          </li>
+          <li>
+            <AnimLink className={styles.link} to={"/experiencias-musicales"}>
+              Experiencias Musicales
+            </AnimLink>
+          </li>
+          <li>
+            <AnimLink className={styles.link} to={"/paquetes"}>
+              Paquetes
+            </AnimLink>
+          </li>
+          <li>
+            <AnimLink className={styles.link} to={"/preguntas-frecuentes"}>
+              Preguntas Frecuentes
+            </AnimLink>
+          </li>
+          <li>
+            <AnimLink className={styles.link} to={"/contactenos"}>
+              Contáctenos
+            </AnimLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { createRef, useEffect } from "react";
 import Header from "./components/Header";
+import * as styles from "./Layout.scss";
+
 const Layout = (props) => {
+  const scrollReferenceRef = createRef();
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {});
+  }, []);
   return (
     <div>
+      <div className={styles.scrollReference} ref={scrollReferenceRef}></div>
       <Header />
       <main style={{ height: "200vh" }}>{props.children}</main>
     </div>
