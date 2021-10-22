cdc97094f5e9b473bb6abcd3ddad2c84d5564237
alejandro <avenegasc14@gmail.com>
2021-10-21 19:36:16 -0600

    Move route observer to header

--- src/components/Layout/components/Header/Header.js
+++ src/components/Layout/components/Header/Header.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import * as styles from "./Header.module.scss";
 import { StaticImage } from "gatsby-plugin-image";
 import AnimLink from "../../../shared/AnimLink/AnimLink";
+import { globalHistory } from "@reach/router";
 const Header = (props) => {
+  const [activeUrl, setActiveUrl] = useState("");
+
+  const checkLocation = () => {
+    setActiveUrl(window.location.pathname);
+  };
+  useEffect(() => {
+    checkLocation();
+    globalHistory.listen(({ action }) => {
+      checkLocation();
+    });
+  }, []);
   return (
     <header
       className={styles.header}
@@ -30,37 +42,61 @@ const Header = (props) => {
       <nav className={styles.nav}>
         <ul className={styles.list}>
           <li>
-            <AnimLink className={styles.link} to={"/"}>
+            <AnimLink activeUrl={activeUrl} className={styles.link} to={"/"}>
               Inicio
             </AnimLink>
           </li>
           <li>
-            <AnimLink className={styles.link} to={"/quienes-somos"}>
+            <AnimLink
+              activeUrl={activeUrl}
+              className={styles.link}
+              to={"/quienes-somos"}
+            >
               ¿Quiénes Somos?
             </AnimLink>
           </li>
           <li>
-            <AnimLink className={styles.link} to={"/docentes"}>
+            <AnimLink
+              activeUrl={activeUrl}
+              className={styles.link}
+              to={"/docentes"}
+            >
               Docentes
             </AnimLink>
           </li>
           <li>
-            <AnimLink className={styles.link} to={"/experiencias-musicales"}>
+            <AnimLink
+              activeUrl={activeUrl}
+              className={styles.link}
+              to={"/experiencias-musicales"}
+            >
               Experiencias Musicales
             </AnimLink>
           </li>
           <li>
-            <AnimLink className={styles.link} to={"/paquetes"}>
+            <AnimLink
+              activeUrl={activeUrl}
+              className={styles.link}
+              to={"/paquetes"}
+            >
               Paquetes
             </AnimLink>
           </li>
           <li>
-            <AnimLink className={styles.link} to={"/preguntas-frecuentes"}>
+            <AnimLink
+              activeUrl={activeUrl}
+              className={styles.link}
+              to={"/preguntas-frecuentes"}
+            >
               Preguntas Frecuentes
             </AnimLink>
           </li>
           <li>
-            <AnimLink className={styles.link} to={"/contactenos"}>
+            <AnimLink
+              activeUrl={activeUrl}
+              className={styles.link}
+              to={"/contactenos"}
+            >
               Contáctenos
             </AnimLink>
           </li>
--- src/components/shared/AnimLink/AnimLink.js
+++ src/components/shared/AnimLink/AnimLink.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { globalHistory } from "@reach/router";
+import React from "react";
 
 import TransitionLink from "gatsby-plugin-transition-link";
 import anime from "animejs";
@@ -8,26 +7,14 @@ import * as styles from "./AnimLink.module.scss";
 
 const AnimLink = (props) => {
   const durationInSeconds = 0.25;
-  const [className, setClassName] = useState(styles.link);
-  useEffect(() => {
-    if (props.to === window.location.pathname) {
-      setClassName(`${styles.link} ${styles.selected}`);
-    } else {
-      setClassName(styles.link);
-    }
-    return globalHistory.listen(({ action }) => {
-      console.log(window.location.pathname);
-      if (props.to === window.location.pathname) {
-        setClassName(`${styles.link} ${styles.selected}`);
-      } else {
-        setClassName(styles.link);
-      }
-    });
-  }, []);
+  const isActive =
+    props.activeUrl.replaceAll("/", "") === props.to.replaceAll("/", "");
   return (
     <TransitionLink
       to={props.to}
-      className={className + " " + props.className || ""}
+      className={`${styles.link} ${props.className} ${
+        isActive && styles.selected
+      }`}
       exit={{
         length: durationInSeconds,
         trigger: ({ node, e, exit, entry }) => {
