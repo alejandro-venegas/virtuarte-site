bf39ea470845f52190e5ec875ee471dc8b5ec27a
alejandro <avenegasc14@gmail.com>
2021-10-25 08:13:47 -0600

    Connect with DatoCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
@@ -8,6 +11,24 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: process.env.DATO_CMS_API,
+
+        // The project environment to read from. Defaults to the primary environment:
+        environment: `main`,
+
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+      },
+    },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
