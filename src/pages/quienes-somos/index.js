86a0cf7727c2c67dc17dbdf768dd07a9554f791d
alejandro <avenegasc14@gmail.com>
2021-11-05 23:01:56 -0600

    Add quienes-somos content and some new pages

--- src/pages/quienes-somos/index.js
+++ src/pages/quienes-somos/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import * as styles from "./QuienesSomos.module.scss";
 import ParallaxTitle from "../../components/shared/ParallaxTitle/ParallaxTitle";
 import { StaticImage } from "gatsby-plugin-image";
 import { Helmet } from "react-helmet";
+import ContentWrapper from "../../components/shared/ContentWrapper/ContentWrapper";
+import procesoEducativoImg from "../../images/quienes-somos/proceso_educativo.svg";
+import AnimLink from "../../components/shared/AnimLink/AnimLink";
 const QuienesSomos = () => {
   return (
-    <div>
+    <section>
       <Helmet>
         <title>¿Quiénes somos?</title>
       </Helmet>
@@ -24,8 +27,54 @@ const QuienesSomos = () => {
           alt={""}
         ></StaticImage>
       </ParallaxTitle>
-      <div>Quienes Somos page</div>
-    </div>
+      <section>
+        <ContentWrapper>
+          <h2>¿Qué es Virtuarte?</h2>
+          <p style={{ textAlign: "center", width: "940px" }}>
+            Es una oportunidad para acceder de manera virtual al aprendizaje de
+            la música. No solamente desde un referente meramente académico, sino
+            desde las distintas virtudes que conforman a cada persona; buscando
+            la manera más eficaz de aprender mediante herramientas digitales.{" "}
+          </p>
+        </ContentWrapper>
+        <div className={styles.backgroundColor}>
+          <ContentWrapper>
+            <div className={styles.misionVisionContainer}>
+              <section>
+                <h3>Misión:</h3>
+                <p>
+                  Promover una educación musical virtual que potencie al máximo
+                  las virtudes de cada persona, de acuerdo a sus intereses y
+                  objetivos; las cuales se desarrollarán mediante actividades,
+                  herramientas y plataformas digitales.
+                </p>
+              </section>
+              <section>
+                <h3>Visión:</h3>
+                <p>
+                  Ser un proveedor artístico por excelencia de una educación
+                  virtual asequible.
+                </p>
+              </section>
+            </div>
+          </ContentWrapper>
+        </div>
+      </section>
+      <section>
+        <ContentWrapper>
+          <h2>¿Cuál es nuestra metodología?</h2>
+          <div className={styles.metodologiaContainer}>
+            <AnimLink
+              to={"./proceso-educativo"}
+              className={styles.metodologiaItem}
+            >
+              <img src={procesoEducativoImg} alt="Proceso Educativo" />
+              <p className={`h3 ${styles.h3}`}>Proceso Educativo</p>
+            </AnimLink>
+          </div>
+        </ContentWrapper>
+      </section>
+    </section>
   );
 };
 
---
+++ src/pages/quienes-somos/proceso-educativo/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import ParallaxTitle from "../../../components/shared/ParallaxTitle/ParallaxTitle";
+import { StaticImage } from "gatsby-plugin-image";
+
+const ProcesoEducativo = () => {
+  return (
+    <div>
+      <ParallaxTitle
+        src={"experiencias_musicales_hero.jpg"}
+        height={"340px"}
+        alt={""}
+        overlayColor={"rgba(33,33,33,0.3)"}
+        title={"Preguntas Frecuentes"}
+        titleStyle={{ color: "white", fontSize: "72px" }}
+      >
+        <StaticImage
+          backgroundColor={"rgb(97, 170, 165)"}
+          src={"../../images/experiencias_musicales_hero.jpg"}
+          alt={"Hero"}
+        />
+      </ParallaxTitle>
+    </div>
+  );
+};
+
+export default ProcesoEducativo;
