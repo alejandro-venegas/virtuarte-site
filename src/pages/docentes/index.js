4e60fca9c04199c7f58a2f9e69ab2527c9d7aa11
Alejandro Venegas <avenegasc14@gmail.com>
2021-12-02 23:35:17 -0600

    Add some changes to docentes

--- src/pages/docentes/index.js
+++ src/pages/docentes/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+
+import * as styles from "./Docentes.module.scss";
 
 const Docentes = () => {
-  return <div>Docentes Page</div>;
+  return (
+    <section style={{ marginTop: "100px" }}>
+      <Helmet>
+        <title>Docentes</title>
+      </Helmet>
+      <h1 className={styles.title}>¡Conocé nuestros docentes!</h1>
+    </section>
+  );
 };
 
 export default Docentes;
