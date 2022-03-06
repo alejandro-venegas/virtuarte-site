62414d285e5c11b7d892d8e733a7e1aa76b4ff3d
Alejandro Venegas <avenegasc14@gmail.com>
2022-03-05 23:10:18 -0600

    Add contactenos form

--- src/pages/contactenos/index.js
+++ src/pages/contactenos/index.js
@@ -4,7 +4,7 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const Contactenos = () => {
   return (
-    <div>
+    <section>
       <ParallaxTitle
         overlayColor={"rgba(33,33,33,0.6)"}
         title={"Contáctenos"}
@@ -18,7 +18,17 @@ const Contactenos = () => {
           alt={"Hero"}
         />
       </ParallaxTitle>
-    </div>
+      <iframe
+        jsname="L5Fo6c"
+        className="YMEQtf DnR2hf"
+        sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals"
+        aria-label="Google Forms, Envíanos un mensaje"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdO-_n49FUkjDYrbDmZDETf7jdQLksjOjBQi5V5oTIxTAUfCA/viewform?embedded=true"
+        allowFullScreen=""
+        frameBorder="0"
+        style={{ width: "50%", height: "800px" }}
+      ></iframe>
+    </section>
   );
 };
 
