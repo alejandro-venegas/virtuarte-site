83d75da4d0a6692886a7482c942205aa952eb190
Alejandro Venegas <avenegasc14@gmail.com>
2023-02-15 21:17:41 -0600

    Fix email form handler

--- src/api/email-subscription.js
+++ src/api/email-subscription.js
@@ -5,14 +5,24 @@ client.setConfig({
   server: process.env.MAILCHIMP_SERVER,
 });
 
+const listId = "c9c3135bc9";
+
 export default async function formHandler(req, res) {
   const email = req.body.email;
 
   try {
-    const response = await client.lists.addListMember("c9c3135bc9", {
+    const memberInfo = await getMemberInfo(email);
+    if (memberInfo.status === "subscribed") {
+      return res.status(400).json({ title: "Member Exists" });
+    }
+
+    const response = await client.lists.setListMember(listId, email, {
       email_address: email,
+      status_if_new: "subscribed",
       status: "subscribed",
     });
+
+    console.log("hello");
 
     console.log(response);
 
@@ -21,5 +31,19 @@ export default async function formHandler(req, res) {
     const data = JSON.parse(error.response.text);
     console.log(data);
     return res.status(400).json(data);
+  }
+}
+
+async function getMemberInfo(email) {
+  try {
+    const info = await client.lists.getListMember(listId, email);
+
+    return info;
+  } catch (e) {
+    if (e.status === 404) {
+      return null;
+    }
+
+    throw { response: { text: "Server Error" } };
   }
 }
